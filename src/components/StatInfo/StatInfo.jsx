2791908d2584e362a673040471f71fe53d83dd5d
Rescor <rescorent@gmail.com>
2024-10-24 20:51:05 +0200

    Move online listeners counter to debug options

--- src/components/StatInfo/StatInfo.jsx
+++ src/components/StatInfo/StatInfo.jsx
@@ -1,12 +1,14 @@
 import StatElem from "./StatElem/StatElem";
-import styles from './StatInfo.module.css';
 import NoneSelect from "../UI/NoneSelect";
+import styles from './StatInfo.module.css';
+
+const DEBUG = localStorage.getItem('debug');
 
 export default function StatInfo({ data }) {
   return <div className={styles.stat_container}>
     <StatElem data={data?.live?.streamer_name || 'AutoDJ'} icon='mic' />
-    <NoneSelect>
+    {DEBUG && <NoneSelect>
       <StatElem data={data?.listeners?.current || 0} icon='headphones' />
-    </NoneSelect>
+    </NoneSelect>}
   </div>
 }
